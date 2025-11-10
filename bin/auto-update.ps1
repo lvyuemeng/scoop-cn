e39a1855a17af72735efbddcc674771237c5fad9
lvyuemeng <lv15623448685@gmail.com>
2025-11-10 10:20:50 +0800

    fix: fix the escaping twice problem by change format from json to ps1

--- bin/auto-update.ps1
+++ bin/auto-update.ps1
@@ -3,89 +3,105 @@ param (
 	[Switch]$DryRun
 )
 
-# --- PART 1: AGGREGATION ---
-Write-Host "Starting bucket aggregation..."
-
-$BinRoot = $PSScriptRoot
-$Root = (Get-Item $PSScriptRoot).Parent.FullName
-$BucketDir = Join-Path $Root "bucket"
-$ScriptsDir = Join-Path $Root "scripts"
+$BIN_ROOT = $PSScriptRoot
+$ROOT = (Get-Item $PSScriptRoot).Parent.FullName
+$BUCKET_DIR = Join-Path $ROOT "bucket"
+$SCRIPTS_DIR = Join-Path $ROOT "scripts"
 # Safety: $Root\config.json = <repo>\bin\config.json
-$Context = Get-Content -Path ("$BinRoot\config.json") -Raw | ConvertFrom-Json
-Write-Host "Found $($Context.repositories.Count) official repositories."
+$Context = & "$BIN_ROOT\config.ps1"
 
-# Clear old buckets
-Remove-Item -Path $BucketDir -Recurse -Force -ErrorAction SilentlyContinue
-Remove-Item -Path $ScriptsDir -Recurse -Force -ErrorAction SilentlyContinue
-New-Item -ItemType Directory -Path $BucketDir
-New-Item -ItemType Directory -Path $ScriptsDir
+# --- PART 1: AGGREGATION ---
+function aggregation {
+	param(
+		$repositories
+	)
 
-# List buckets in context
-$Buckets = $Context.repositories | ForEach-Object {
-	$_.Split('/')[-1]
-}
+	Write-Host "Starting bucket aggregation..."
+	Write-Host "Found $($repositories.Count) official repositories."
 
-# Safety: Our repo contains all pulled buckets repo.
-foreach ($bucket in $Buckets) {
-	Write-Verbose "Aggregating $bucket..."
-	$SourceBucket = Join-Path $Root $bucket "bucket"
-	$SourceScripts = Join-Path $Root $bucket "scripts"
-    
-	if (Test-Path $SourceBucket) {
-		Copy-Item -Path (Join-Path $SourceBucket "*") -Destination $BucketDir -Recurse -Force
+	# Clear old buckets
+	Remove-Item -Path $BUCKET_DIR -Recurse -Force -ErrorAction SilentlyContinue
+	Remove-Item -Path $SCRIPTS_DIR -Recurse -Force -ErrorAction SilentlyContinue
+	New-Item -ItemType Directory -Path $BUCKET_DIR
+	New-Item -ItemType Directory -Path $SCRIPTS_DIR
+
+	# List buckets in context
+	$buckets = $repositories | ForEach-Object {
+		$_.Split('/')[-1]
 	}
-	if (Test-Path $SourceScripts) {
-		Copy-Item -Path (Join-Path $SourceScripts "*") -Destination $ScriptsDir -Recurse -Force
-	}
-}
-Write-Host "Aggregation complete."
 
-# --- PART 2: URL REPLACEMENT ---
-Write-Host "Loading and applying replacement rules..."
-# Load and filter for only enabled rules
-$rules = $Context.rules | Where-Object { $_.enabled -eq $true }
-
-if ($rules.Count -eq 0) {
-	Write-Warning "No enabled rules found in config.json. Skipping replacement."
-} else {
-	Write-Host "Applying $($rules.Count) enabled replacement rules..."
-}
-
-$manifests = Get-ChildItem -Recurse -Path $BucketDir -Filter "*.json"
-
-foreach ($man in $manifests) {
-	$content = Get-Content -Path $man.FullName -Raw
-	$isChange = $false
-
-	# Apply all enabled rules
-	foreach ($rule in $rules) {
-		if ($content -match $rule.find) {
-			Write-Verbose "[$($man.Name)] Applying rule: $($rule.description)"
-			# the find string is already escaped in `ConvertFrom-Json`, so we need to unescape it
-			# replace 'pattern' 'with'
-			# 'pattern': escaped
-			# 'with': unescaped
-			# replace '\\' '\\': '\' -> '\\'
-			$content = $content -replace ($rule.find -replace '\\', '\\'), $rule.replace
-			$isChange = $true
+	# Safety: Our repo contains all pulled buckets repo.
+	foreach ($bucket in $buckets) {
+		Write-Verbose "Aggregating $bucket..."
+		$SourceBucket = Join-Path $Root $bucket "bucket"
+		$SourceScripts = Join-Path $Root $bucket "scripts"
+		
+		if (Test-Path $SourceBucket) {
+			Copy-Item -Path (Join-Path $SourceBucket "*") -Destination $BUCKET_DIR -Recurse -Force
+		}
+		if (Test-Path $SourceScripts) {
+			Copy-Item -Path (Join-Path $SourceScripts "*") -Destination $SCRIPTS_DIR -Recurse -Force
 		}
 	}
+	Write-Host "Aggregation complete."
+}
 
-	# Write the file back *only* if changes were made
-	if ($isChange) {
-		if ($DryRun) {
-			Write-Warning "DRY RUN: Would have modified $($man.FullName)"
-		} else {
-			Write-Host "Updating $($man.FullName)"
-			Set-Content -Path $man.FullName -Value $content -NoNewline -Encoding utf8NoBOM
+# --- PART 2: URL REPLACEMENT ---
+function replacement {
+	param(
+		$rules
+	)
+	Write-Host "Loading and applying replacement rules..."
+	# Load and filter for only enabled rules
+	$rules = $rules | Where-Object { $_.enabled -eq $true }
+
+	if ($rules.Count -eq 0) {
+		Write-Warning "No enabled rules found in config.json. Skipping replacement."
+	} else {
+		Write-Host "Applying $($rules.Count) enabled replacement rules..."
+	}
+
+	$manifests = Get-ChildItem -Recurse -Path $BUCKET_DIR -Filter "*.json"
+
+	foreach ($man in $manifests) {
+		$content = Get-Content -Path $man.FullName -Raw
+		$isChange = $false
+
+		# Apply all enabled rules
+		foreach ($rule in $rules) {
+			if ($content -match $rule.find) {
+				Write-Verbose "[$($man.Name)] Applying rule: $($rule.description)"
+				$content = $content -replace $rule.find, $rule.replace
+				$isChange = $true
+			}
+		}
+
+		# Write the file back *only* if changes were made
+		if ($isChange) {
+			if ($DryRun) {
+				Write-Warning "DRY RUN: Would have modified $($man.FullName)"
+			} else {
+				Write-Host "Updating $($man.FullName)"
+				Set-Content -Path $man.FullName -Value $content -NoNewline -Encoding utf8NoBOM
+			}
 		}
 	}
 }
 
 # --- PART 3: CLEANUP ---
-Write-Host "Cleaning up source bucket folders..."
-foreach ($bucket in $Buckets) {
-	Remove-Item -Path (Join-Path $Root $bucket) -Recurse -Force
+function cleanup {
+	param(
+		$repositories
+	)
+
+	Write-Host "Cleaning up source bucket folders..."
+	$repositories | ForEach-Object {
+		$bucket = $_.Split('/')[-1]
+		Remove-Item -Path (Join-Path $ROOT $bucket) -Recurse -Force
+	}
 }
 
+aggregation $Context.repositories
+replacement $Context.rules
+cleanup $Context.repositories
 Write-Host "Update process complete."
---
+++ bin/config.ps1
@@ -0,0 +1,203 @@
+@{
+	repositories = @(
+		"ScoopInstaller/Main",
+		"ScoopInstaller/Extras",
+		"ScoopInstaller/Versions",
+		"ScoopInstaller/Nonportable",
+		"ScoopInstaller/Java",
+		"ScoopInstaller/PHP",
+		"kodybrown/scoop-nirsoft",
+		"matthewjberger/scoop-nerd-fonts",
+		"Calinou/scoop-games",
+		"niheaven/scoop-sysinternals"
+	)
+	
+	rules = @(
+		@{
+			description = "Proxy: GitHub Releases Download"
+			find = '(https?://github\.com/.+/releases/.*download)'
+			replace = 'https://gh-proxy.com/$1'
+			enabled = $true
+		},
+		@{
+			description = "Proxy: GitHub Archive"
+			find = '(https?://github\.com/.+/archive/)'
+			replace = 'https://gh-proxy.com/$1'
+			enabled = $true
+		},
+		@{
+			description = "Proxy: GitHub Gists"
+			find = '(https?://gist\.github\.com/.+/)'
+			replace = 'https://gh-proxy.com/$1'
+			enabled = $true
+		},
+		@{
+			description = "Proxy: GitHub Raw (raw.githubusercontent.com)"
+			find = '(https?://raw\.githubusercontent\.com)'
+			replace = 'https://gh-proxy.com/$1'
+			enabled = $true
+		},
+		@{
+			description = "Proxy: GitHub Raw (github.com/user/repo/raw/)"
+			find = '(https?://github\.com/.+/raw/)'
+			replace = 'https://gh-proxy.com/$1'
+			enabled = $true
+		},
+		@{
+			description = "Proxy: DBeaver (to GitHub Releases)"
+			find = 'https?://dbeaver\.io/files/([\d\.]+)/'
+			replace = 'https://gh-proxy.com/https://github.com/dbeaver/dbeaver/releases/download/$1/'
+			enabled = $true
+		},
+		@{
+			description = "Proxy: FastCopy (to GitHub Raw)"
+			find = 'https?://fastcopy\.jp/archive'
+			replace = 'https://gh-proxy.com/https://raw.githubusercontent.com/FastCopyLab/FastCopyDist2/main'
+			enabled = $true
+		},
+		@{
+			description = "Proxy: OBS Studio (cdn-fastly) to GitHub Releases"
+			find = 'https?://cdn-fastly\.obsproject\.com/downloads/OBS-Studio-(.+)-Windows'
+			replace = 'https://gh-proxy.com/https://github.com/obsproject/obs-studio/releases/download/$1/OBS-Studio-$1-Windows'
+			enabled = $true
+		},
+		@{
+			description = "Proxy: OBS Studio 2.7 (cdn-fastly) to GitHub Releases"
+			find = 'https?://cdn-fastly\.obsproject\.com/downloads/OBS-Studio-(.+)-Full'
+			replace = 'https://gh-proxy.com/https://github.com/obsproject/obs-studio/releases/download/$1/OBS-Studio-$1-Full'
+			enabled = $true
+		},
+		@{
+			description = "Proxy: Strawberry Music Player to GitHub Releases"
+			find = 'https?://files\.jkvinge\.net/packages/strawberry/StrawberrySetup-(.+)-mingw-x'
+			replace = 'https://gh-proxy.com/https://github.com/strawberrymusicplayer/strawberry/releases/download/$1/StrawberrySetup-$1-mingw-x'
+			enabled = $true
+		},
+		@{
+			description = "Proxy: 7-Zip (to GitHub Releases)"
+			find = 'https?://www\.7-zip\.org/a/7z(\d{2})(\d{2})'
+			replace = 'https://gh-proxy.com/https://github.com/ip7z/7zip/releases/download/$1.$2/7z$1$2'
+			enabled = $true
+		},
+		@{
+			description = "Mirror: Blender"
+			find = 'download\.blender\.org'
+			replace = 'mirrors.tuna.tsinghua.edu.cn/blender'
+			enabled = $true
+		},
+		@{
+			description = "Mirror: Cygwin"
+			find = '//.*/cygwin/'
+			replace = '//mirrors.tuna.tsinghua.edu.cn/cygwin/'
+			enabled = $true
+		},
+		@{
+			description = "Mirror: GIMP"
+			find = 'download\.gimp\.org/mirror/pub'
+			replace = 'mirrors.nju.edu.cn/gimp'
+			enabled = $true
+		},
+		@{
+			description = "Mirror: Go"
+			find = 'dl\.google\.com/go'
+			replace = 'mirrors.nju.edu.cn/golang'
+			enabled = $true
+		},
+		@{
+			description = "Mirror: Gradle"
+			find = 'services\.gradle\.org/distributions'
+			replace = 'mirror.nju.edu.cn/gradle'
+			enabled = $true
+		},
+		@{
+			description = "Mirror: Inkscape"
+			find = 'media\.inkscape\.org/dl/resources/file'
+			replace = 'mirrors.nju.edu.cn/inkscape'
+			enabled = $true
+		},
+		@{
+			description = "Mirror: Kodi"
+			find = 'mirrors\.kodi\.tv'
+			replace = 'mirrors.tuna.tsinghua.edu.cn/kodi'
+			enabled = $true
+		},
+		@{
+			description = "Mirror: LaTeX, MiKTeX (CTAN)"
+			find = '(miktex\.org/download/ctan)|(mirrors.+/CTAN)'
+			replace = 'mirrors.tuna.tsinghua.edu.cn/CTAN'
+			enabled = $true
+		},
+		@{
+			description = "Mirror: Node"
+			find = 'nodejs\.org/dist'
+			replace = 'mirrors.ustc.edu.cn/node'
+			enabled = $true
+		},
+		@{
+			description = "Mirror: Python"
+			find = 'www\.python\.org/ftp/python'
+			replace = 'mirrors.nju.edu.cn/python'
+			enabled = $true
+		},
+		@{
+			description = "Mirror: Vim"
+			find = 'ftp\.nluug\.nl/pub/vim/pc'
+			replace = 'mirrors.nju.edu.cn/vim/pc'
+			enabled = $true
+		},
+		@{
+			description = "Mirror: VirtualBox"
+			find = 'download\.virtualbox\.org/virtualbox'
+			replace = 'mirrors.tuna.tsinghua.edu.cn/virtualbox'
+			enabled = $true
+		},
+		@{
+			description = "Mirror: VLC"
+			find = 'download\.videolan\.org/pub'
+			replace = 'mirrors.tuna.tsinghua.edu.cn/videolan-ftp'
+			enabled = $true
+		},
+		@{
+			description = "Mirror: Wireshark"
+			find = 'www\.wireshark\.org/download'
+			replace = 'mirrors.tuna.tsinghua.edu.cn/wireshark'
+			enabled = $true
+		},
+		@{
+			description = "Mirror: Lunacy (Icons8)"
+			find = 'lun-eu\.icons8\.com/s/'
+			replace = 'lcdn.icons8.com/'
+			enabled = $true
+		},
+		@{
+			description = "Mirror: Tor Browser, Tor"
+			find = 'archive\.torproject\.org/tor-package-archive'
+			replace = 'tor.calyxinstitute.org/dist'
+			enabled = $true
+		},
+		@{
+			description = "Mirror: Typora"
+			find = 'download\.typora\.io'
+			replace = 'downloads.typoraio.cn'
+			enabled = $true
+		},
+		@{
+			description = "Fix: Internal 'scripts' paths"
+			find = '(bucketsdir\\\\).+(\\\\scripts)'
+			replace = '$1scoop-cn$2'
+			enabled = $true
+		},
+		@{
+			description = "Fix: Internal 'suggest' paths"
+			find = '\"main/|\"extras/|\"versions/|\"nirsoft/|\"sysinternals/|\"php/|\"nerd-fonts/|\"nonportable/|\"java/|\"games/'
+			replace = '"scoop-cn/'
+			enabled = $true
+		},
+		@{
+			description = "Fix: Internal 'depends' paths"
+			find = '\"depends\":\s*\"(scoop\-cn/)?'
+			replace = '"depends": "scoop-cn/'
+			enabled = $true
+		}
+	)
+}
